06c94218c0ae1635ebb94f64cfb1d82ff80c112f
katawes <katawes@gmail.com>
2019-06-29 01:09:24 +0200

    Updates

---
+++ precache-manifest.dea006bc9cbdfd04f5805ed4f5fc9061.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167a8c65371d09e0ee1635c41e0f76c8",
+    "url": "/list-of-expenses/index.html"
+  },
+  {
+    "revision": "f2feea3925c93066db06",
+    "url": "/list-of-expenses/static/css/main.0b9b8364.chunk.css"
+  },
+  {
+    "revision": "bb585c9e5dbd8e10e03b",
+    "url": "/list-of-expenses/static/js/2.00ac8801.chunk.js"
+  },
+  {
+    "revision": "f2feea3925c93066db06",
+    "url": "/list-of-expenses/static/js/main.2f01679a.chunk.js"
+  },
+  {
+    "revision": "bdc79c93c7b4268cbbaf",
+    "url": "/list-of-expenses/static/js/runtime~main.ea80d9c8.js"
+  }
+]);
